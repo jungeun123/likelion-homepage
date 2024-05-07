e18a3410faf868412b31152431ff564eba42cd63
jungeun123 <147065885+jungeun123@users.noreply.github.com>
2024-05-07 21:21:21 +0900

    Feat: SimpleTestController에 @RequestMapping 추가

--- src/main/java/com/homepage/likelion/test/SimpleTestController.java
+++ src/main/java/com/homepage/likelion/test/SimpleTestController.java
@@ -1,14 +1,22 @@
 package com.homepage.likelion.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/simpleText")
 public class SimpleTestController {
 
     // GET http://localhost:8080/api/simpleText/success
-    @GetMapping("/api/simpleText/success")
+    @GetMapping("/success")
     public String simpleTextSuccess(){
+        return "안녕";
+    }
+
+    // GET http://localhost:8080/api/simpleText/success
+    @GetMapping("/fail")
+    public String simpleTextFail(){
         return "안녕";
     }
 
